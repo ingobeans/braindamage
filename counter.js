6de23e9ccb08ee87555b2def1b5d6f63373bb453
ingobeans <127751491+ingobeans@users.noreply.github.com>
2026-01-19 11:39:16 +0100

    make hit counter not pause page load

---
+++ counter.js
@@ -0,0 +1,12 @@
+// script to load the page hitcounter from https://www.websiteout.net/counter.php
+// 
+// i cant use the provided script directly, as it pauses page loading to execute. 
+// it also cant be ran deferred, since it will try to inject the page counter at its own location.
+// instead we let the script run at the bottom of the html, injecting the page counter in to a display:none div.
+// then, this script moves the created counter in to the "braindamage-counter" element.
+
+let parent = document.getElementById("braindamage-counter");
+let holder = document.getElementById("counter-holder");
+let counter = holder.children[holder.children.length - 1];
+
+parent.appendChild(counter);
